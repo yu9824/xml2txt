fe7dd06c1349bd1a47f304b86e29616f1f94cbfc
yu9824 <58211916+yu9824@users.noreply.github.com>
2021-09-18 14:21:29 +0900

    Update __app_name__

--- xml2txt/__init__.py
+++ xml2txt/__init__.py
@@ -6,7 +6,7 @@ __author__       = 'yu9824'
 __copyright__    = 'Copyright © 2021 yu9824'
 __author_email__ = 'yu.9824.job@gmail.com'
 __user_id__ = 'yu9824'
-__app_name__ = 'template'
+__app_name__ = 'xml2txt'
 __url__ = 'http://github.com/yu9824/xml2txt/'
 
 __all__ = [
