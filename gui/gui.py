1a799926976aaf1c3f7bb061c307a2f0c9b012b1
yu9824 <58211916+yu9824@users.noreply.github.com>
2021-09-18 14:20:32 +0900

    Update gui

--- gui/gui.py
+++ gui/gui.py
@@ -5,16 +5,20 @@ import sys
 sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), '..'))
 from xml2txt import *
 
-window_options = {}
-window_options['font'] = 'Helvetica 18'
+window_options = dict(
+    font = 'Helvetica 18'
+)
 
 main = {}
 text_size = (18, 1)
 
+key_input = '-INPUTFILES-'
+key_output = '-OUTPUTFOLDER-'
+
 main['layout'] = [
     # [sg.Text('xml → txt (diffraction)')],
-    [sg.Text('input files (.xml)', size = text_size), sg.Input('', key = '-INPUTFILES-'), sg.FilesBrowse(initial_folder = os.path.join(os.environ['HOME'], 'Desktop'))],
-    [sg.Text('output folder', size = text_size), sg.Input(os.path.join(os.environ['HOME'], 'Desktop'), key = '-OUTPUTFOLDER-'), sg.FolderBrowse(initial_folder = os.path.join(os.environ['HOME'], 'Desktop'))],
+    [sg.Text('input files (.xml)', size = text_size), sg.Input(key = key_input, enable_events=True), sg.FilesBrowse(file_types=(('PDF-card(xml)', '.xml'),))],
+    [sg.Text('output folder', size = text_size), sg.Input(key= key_output), sg.FolderBrowse()],
     [sg.OK(), sg.Cancel()]
 ]
 
@@ -24,11 +28,25 @@ while True:
     main['event'], main['values'] = main['window'].read()
     if main['event'] == 'Cancel' or main['event'] is None:
         break
+    elif main['event'] == key_input:
+        if main['values'][key_output] == '':
+            main['window'][key_output].Update(os.path.dirname(main['values'][key_input]))
     else:
-        for file in main['values']['-INPUTFILES-'].split(';'):
-            df = xml2df(file)
-            outputfilepath = os.path.join(main['values']['-OUTPUTFOLDER-'], os.path.splitext(os.path.basename(file))[0] + 'txt')
-            df.to_csv(outputfilepath, encoding = 'utf_8_sig', index = False)
+        for fpath_input in main['values'][key_input].split(';'):
+            # check file
+            if os.path.isfile(fpath_input):
+                df = xml2df(fpath_input)
+            else:
+                sg.PopupError('FileNotFound', **window_options)
+            
+            # create output file path
+            fpath_output = os.path.join(main['values'][key_output], os.path.splitext(os.path.basename(fpath_input))[0] + '.txt')
+            if os.path.isfile(fpath_output):
+                flag_overwrite = sg.popup_yes_no('Overwrite?', **window_options)
+            if flag_overwrite == 'Yes':
+                df.to_csv(fpath_output, encoding = 'utf_8_sig', index = False)
+            else:
+                continue
             sg.popup_ok('Finished!', **window_options)
 main['window'].close()
 
