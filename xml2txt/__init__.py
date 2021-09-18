cb2528144c480f83a26e7a3751d3b90a09cefe1c
yu9824 <58211916+yu9824@users.noreply.github.com>
2021-09-18 13:57:42 +0900

    Update __init__.py

--- xml2txt/__init__.py
+++ xml2txt/__init__.py
@@ -1,1 +1,14 @@
 from .xml2txt import *
+
+__version__      = '0.0.2dev'
+__license__      = 'LGPL3'
+__author__       = 'yu9824'
+__copyright__    = 'Copyright © 2021 yu9824'
+__author_email__ = 'yu.9824.job@gmail.com'
+__user_id__ = 'yu9824'
+__app_name__ = 'template'
+__url__ = 'http://github.com/yu9824/xml2txt/'
+
+__all__ = [
+    'xml2df',
+    ]
